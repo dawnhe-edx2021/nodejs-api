f9fce56c0382ad79fd539b8f4d0012a483a66964
dawnhe-edx2021 <83760705+dawnhe-edx2021@users.noreply.github.com>
2021-07-19 00:51:20 +0800

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get("/api", (req, res, next) => {
 });
 
 app.get("/fx-static", (req, res, next) => {
- res.json({"sell": "589.181", "timestamp (live)": "2020-06-17 14:42:34.492974", "buy": "489.1"});
+ res.json({"sell": "589.181", "timestamp": "2020-06-17 14:42:34.492974", "buy": "489.1"});
 });
 
 app.get("/fx", (req, res, next) => {
@@ -25,7 +25,7 @@ app.get("/fx", (req, res, next) => {
  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
  var dateTime = date+' '+time;
 
- res.json({"sell": "687." + rand1, "timestamp": dateTime, "buy": "587." + rand2});
+ res.json({"sell": "687." + rand1, "timestamp (live)": dateTime, "buy": "587." + rand2});
 });
 
 app.get("/random", (req, res, next) => {
